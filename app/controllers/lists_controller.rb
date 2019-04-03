7946c8209e7b297b8cfd8d7cda3f2bb39d9cca3a
Nick-Damico <nickalan82@icloud.com>
2019-04-02 20:37:46 -0400

    Add form partial for lists, new.js.erb file for remote js.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,8 +1,22 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show]
   def index
     @lists = List.all
   end
 
+  def new
+    @list = List.new
+  end
+
+  def create
+    byebug
+  end
+
   def show
   end
+
+  private
+    def set_list
+      @list = List.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  get 'lists/index'
-  get 'lists/show', as: 'list'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'lists#index'
+
+  get '/lists' => 'lists#index'
+
+  get 'lists/new' => 'lists#new', as: 'new_list'
+
+  post 'lists' => 'lists#create'
+
+  get 'lists/:id' => 'lists#show', as: 'list'
+
+  get 'lists/:id/update' => 'lists#update'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
